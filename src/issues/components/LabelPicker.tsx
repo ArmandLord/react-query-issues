3f7b0073427cbd61e95981bf8966f1ad1a5f3389
Armando Perez <armandlord98@gmail.com>
2023-04-04 09:54:35 -0600

    Agrega tanstack de react query

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { githubApi } from "../../api/githubApi";
+import { PropsLabels } from "../interfaces/label";
 
 export const LabelPicker = () => {
+  const getLabels = async (): Promise<PropsLabels> => {
+    const { data } = await githubApi.get("/labels");
+    console.log(data);
+
+    return data;
+  };
+  const { data } = useQuery(["labels query"], getLabels, {
+    refetchOnWindowFocus: false,
+  });
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: "#ffccd3" }}
+      >
+        Primary
+      </span>
     </div>
-  )
-}
+  );
+};
---
+++ src/issues/interfaces/label.ts
@@ -0,0 +1,14 @@
+export interface PropsLabels {
+  id: number;
+  node_id: string;
+  url: string;
+  name: string;
+  color: string;
+  default: boolean;
+  description: Description | null;
+}
+
+export enum Description {
+  Empty = "",
+  PullRequestsThatUpdateADependencyFile = "Pull requests that update a dependency file",
+}
